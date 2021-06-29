47d147dede1eff6fbeb39058efa677e2cbfeb5d2
Deepanjan Nag <subscriptions@deepanjannag.com>
2021-06-29 20:19:51 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let screen = document.querySelector('.screen');
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button =>
+    button.addEventListener('click', e => {
+        //clicked =
+        if (e.target.classList.contains('btn-equal')) {
+            screen.value = Function('"use strict";return (' + screen.value + ')')();
+        }
+        else
+            //clicked CLEAR
+            if (e.target.classList.contains('btn-clear')) {
+                screen.value = '';
+                return;
+            }
+            else {  //Clicked something other than = or CLEAR
+                let clickedButton = e.target.dataset.num;
+                screen.value = screen.value + clickedButton;
+            }
+    })
+);
